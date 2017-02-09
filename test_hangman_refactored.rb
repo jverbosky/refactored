361e056ac9b299aad4e9fe9497a9480d38a5abd6
jverbosk <jverbosk@gmail.com>
2017-02-09 14:22:19 -0500

    added test for margin() and started test for start_game()

--- test_hangman_refactored.rb
+++ test_hangman_refactored.rb
@@ -21,4 +21,18 @@ class TestHangmanRefactored < Minitest::Test
   #   assert_equal(system("clear"), results)
   # end
 
+  # I'll be honest and admit that I expected "\n\n" as the output
+  # Assuming this method returning the argument due to the puts statement
+  # Based on this, will not create tests for other methods that simply output text
+  def test_2_verify_margin_method_outputs_correct_number_of_newlines
+    results = margin(2)
+    assert_equal(2, results)
+  end
+
+  # Need to determine how to tell if a method calls another method
+  def test_3_main_method
+    results = 
+    assert_equal( , results)
+  end
+
 end
